20d7302b218de569402d021f6a9318a423e1d436
rdgupta93 <110840567+rdgupta93@users.noreply.github.com>
2023-02-26 21:54:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const main = async()=>{
+    await mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+    const ProductSchema = new mongoose.Schema({
+        name:String,
+        price:Number
+    });
+    const ProductsModel = mongoose.model('products',ProductSchema);
+    let data = new ProductsModel({name:"m 12",price:12500});
+    let result = await data.save();
+    console.log(result)
+}
+main()
+
+
+
+
